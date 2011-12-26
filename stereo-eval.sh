233151dcf9a71bcf6861411f71544ab20ffd1f08
Michael Koval <koval.michael@gmail.com>
2011-12-26 03:29:36 -0500

    Evaluation script.

---
+++ stereo-eval.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+img_left='images/left.png'
+img_right='images/right.png'
+threads_max=48
+repeats=20
+
+
+for threads in $(seq 1 $threads_max); do
+    export OMP_NUM_THREADS=$threads
+    echo -n "$threads,"
+    ./stereo $img_left $img_right cpu_custom $repeats
+done
