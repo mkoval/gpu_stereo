71fcacb9a0815abd134c557a94cb1b7ae91747e0
Michael Koval <koval.michael@gmail.com>
2011-12-26 15:03:04 -0500

    Compute average using bc.

--- stereo-eval.sh
+++ stereo-eval.sh
@@ -2,16 +2,18 @@
 
 img_left='images/left.png'
 img_right='images/right.png'
-threads_max=48
-repeats=20
+threads_max=2
+repeats=3
 
-
-for threads in $(seq 1 $threads_max); do
+for threads in `seq 1 $threads_max`; do
     export OMP_NUM_THREADS=$threads
-    echo -n $threads
+    sum=0
 
-    for repeat in $(seq 1 $repeats); do
-        echo -n ,
-        ./stereo $img_left $img_right cpu_custom $repeats
+    for repeat in `seq 1 $repeats`; do
+        sample=`./stereo $img_left $img_right cpu_custom $repeats`
+        sum=`echo "$sum + $sample" | bc`
     done
+
+    /bin/echo -n "$threads,"
+    echo "scale=5;$sum / $repeats" | bc
 done
